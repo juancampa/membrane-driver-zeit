345b5156831bf9bca03f82ce7671f1cd6aa88dfd
aranajhonny <jhonnyjosearana@gmail.com>
2018-02-15 17:48:43 -0430

    add file

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ export const Deployment = {
     result = await post(`/v2/now/deployments/${uid}/aliases`, {
       alias: args.alias
     });
-    console.log('statusCode', result.statusCode);
+    console.log('status', result.status);
   },
   async self({ source }) {
     return root.deployments.one({ uid: source.uid });
