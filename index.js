10781f6049f3e6739ded36851fc44af4a4542c56
aranajhonny <jhonnyjosearana@gmail.com>
2018-02-16 14:39:05 -0430

    add file

--- index.js
+++ index.js
@@ -5,60 +5,60 @@ const { root } = program.refs;
 export async function init() {
   // Called when the program is run
   return root.set({
-    // deployments: {},
-    // teams: {},
+    deployments: {},
+    teams: {},
     aliases: {}
   });
 }
 
-// export const DeploymentsCollection = {
-//   async one({ args }) {
-//     const result = await get(`/v2/now/deployments/${args.uid}`);
-//     return result;
-//   },
-//   async items() {
-//     const result = await get(`/v2/now/deployments/`);
-//     return result.deployments;
-//   }
-// };
+export const DeploymentsCollection = {
+  async one({ args }) {
+    const result = await get(`/v2/now/deployments/${args.uid}`);
+    return result;
+  },
+  async items() {
+    const result = await get(`/v2/now/deployments/`);
+    return result.deployments;
+  }
+};
 
-// export let DeploymentsItem = {
-//   self({ source }) {
-//     const { uid } = source;
-//     if (uid === undefined || uid === null) {
-//       return null;
-//     }
-//     return root.deployments.one({ uid: source.uid });
-//   }
-// };
+export let DeploymentsItem = {
+  self({ source }) {
+    const { uid } = source;
+    if (uid === undefined || uid === null) {
+      return null;
+    }
+    return root.deployments.one({ uid: source.uid });
+  }
+};
 
-// export const Deployment = {
-//   async setAlias({ self, args }) {
-//     const { uid } = self.match(root.deployments.one());
-//     if (uid === undefined || uid === null) {
-//       return null;
-//     }
-//     result = await post(`/v2/now/deployments/${uid}/aliases`, {
-//       alias: args.alias
-//     });
-//     console.log(result);
-//     return result.status;
-//   },
-//   async getAliases({ source }) {
-//     const { uid } = source;
-//     if (uid === undefined || uid === null) {
-//       return null;
-//     }
-//     result = await get(`/v2/now/deployments/${uid}/aliases`);
-//     return result.aliases;
-//   },
-//   async self({ source }) {
-//     return root.deployments.one({ uid: source.uid });
-//   },
-//   uid({ source }) {
-//     return source['uid'];
-//   }
-// };
+export const Deployment = {
+  async setAlias({ self, args }) {
+    const { uid } = self.match(root.deployments.one());
+    if (uid === undefined || uid === null) {
+      return null;
+    }
+    result = await post(`/v2/now/deployments/${uid}/aliases`, {
+      alias: args.alias
+    });
+    console.log(result);
+    return result.status;
+  },
+  async getAliases({ source }) {
+    const { uid } = source;
+    if (uid === undefined || uid === null) {
+      return null;
+    }
+    result = await get(`/v2/now/deployments/${uid}/aliases`);
+    return result.aliases;
+  },
+  async self({ source }) {
+    return root.deployments.one({ uid: source.uid });
+  },
+  uid({ source }) {
+    return source['uid'];
+  }
+};
 
 export const AliasesCollection = {
   async one({ args }) {
@@ -67,7 +67,7 @@ export const AliasesCollection = {
     return alias;
   },
   async items() {
-    const result = await get(`/teams/`);
+    const result = await get(`/v2/now/aliases/`);
     return result.aliases;
   }
 };
@@ -81,22 +81,22 @@ export const Alias = {
   }
 };
 
-// export const TeamsCollection = {
-//   async one({ args }) {
-//     const result = await get(`/teams/${args.id}`);
-//     return result;
-//   },
-//   async items() {
-//     const result = await get(`/teams/`);
-//     return result.teams;
-//   }
-// };
+export const TeamsCollection = {
+  async one({ args }) {
+    const result = await get(`/teams/${args.id}`);
+    return result;
+  },
+  async items() {
+    const result = await get(`/teams/`);
+    return result.teams;
+  }
+};
 
-// export const Team = {
-//   async self({ source }) {
-//     return root.teams.one({ id: source.id });
-//   },
-//   id({ source }) {
-//     return source['id'];
-//   }
-// };
+export const Team = {
+  async self({ source }) {
+    return root.teams.one({ id: source.id });
+  },
+  id({ source }) {
+    return source['id'];
+  }
+};
