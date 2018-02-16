68edda82b2bda86308335fc8a355cf5e2c82baa3
aranajhonny <jhonnyjosearana@gmail.com>
2018-02-16 10:11:42 -0430

    add file

--- index.js
+++ index.js
@@ -70,9 +70,19 @@ export const AliasesCollection = {
   }
 };
 
+export let AliasItem = {
+  self({ source }) {
+    const { uid } = source;
+    if (uid === undefined || uid === null) {
+      return null;
+    }
+    return root.aliases.one({ uid: source.uid });
+  }
+};
+
 export const Alias = {
   async self({ source }) {
-    return root.alias.one({ uid: source.uid });
+    return root.aliases.one({ uid: source.uid });
   },
   id({ source }) {
     return source['uid'];
