28ca0e31f3490c6f5233cda7921cd78a963d8c69
aranajhonny <jhonnyjosearana@gmail.com>
2018-03-12 15:39:55 -0430

    add file

--- index.js
+++ index.js
@@ -1,52 +1,52 @@
-import { get, post } from './client'
-import { parse as parseUrl } from 'url'
+import { get, post } from './client';
+import { parse as parseUrl } from 'url';
 
-const { root } = program.refs
+const { root } = program.refs;
 
 export async function init() {
   await root.set({
     deployments: {},
     teams: {},
     aliases: {},
-  })
+  });
 }
 
 export async function parse({ name, value }) {
   switch (name) {
     case 'url': {
-      const resultTeams = await get(`/teams/`)
+      const resultTeams = await get(`/teams/`);
       const uid = resultTeams.teams.map(async (team) => {
         const resultDeployments = await get(
           `/v2/now/deployments?teamId=${team.id}`,
-        )
-        const dep = resultDeployments.deployments.find((d) => d.url === value)
+        );
+        const dep = resultDeployments.deployments.find((d) => d.url === value);
         if (dep) {
-          return dep.uid
-          console.log('dep.uid')
+          return dep.uid;
+          console.log('dep.uid');
         }
-      })
-      console.log(uid)
+      });
+      console.log(uid);
 
       // const { pathname: path } = parseUrl(value, true);
       // const parts = path.split('/');
       // if (parts.length >= 5) {
       //   return root.deployments.one({ uid: parts[4] });
       // }
-      break
+      break;
     }
   }
 }
 
 export const DeploymentsCollection = {
   async one({ args }) {
-    const result = await get(`/v2/now/deployments/${args.uid}`)
-    return result
+    const result = await get(`/v2/now/deployments/${args.uid}`);
+    return result;
   },
   async items() {
-    const result = await get(`/v2/now/deployments/`)
-    return result.deployments
+    const result = await get(`/v2/now/deployments/`);
+    return result.deployments;
   },
-}
+};
 
 // export const ScaleConfiguration = {
 
@@ -54,71 +54,71 @@ export const DeploymentsCollection = {
 
 export let DeploymentsItem = {
   self({ source }) {
-    const { uid } = source
+    const { uid } = source;
     if (uid === undefined || uid === null) {
-      return null
+      return null;
     }
-    return root.deployments.one({ uid: source.uid })
+    return root.deployments.one({ uid: source.uid });
   },
-}
+};
 
 export const Deployment = {
   async setAlias({ self, args }) {
-    const { uid } = self.match(root.deployments.one())
+    const { uid } = self.match(root.deployments.one());
     if (uid === undefined || uid === null) {
-      return null
+      return null;
     }
     result = await post(`/v2/now/deployments/${uid}/aliases`, {
       alias: args.alias,
-    })
-    return result.status
+    });
+    return result.status;
   },
 
   async aliases({ source }) {
-    const { uid } = source
+    const { uid } = source;
     if (uid === undefined || uid === null) {
-      return null
+      return null;
     }
-    result = await get(`/v2/now/deployments/${uid}/aliases`)
-    return result.aliases
+    result = await get(`/v2/now/deployments/${uid}/aliases`);
+    return result.aliases;
   },
 
   async self({ source }) {
-    return root.deployments.one({ uid: source.uid })
+    return root.deployments.one({ uid: source.uid });
   },
-}
+};
 
 export const AliasesCollection = {
   async one({ args }) {
-    result = await get(`/v2/now/aliases/`)
-    const alias = result.aliases.find((one) => one.uid === args.uid)
-    return alias
+    result = await get(`/v2/now/aliases/`);
+    const alias = result.aliases.find((one) => one.uid === args.uid);
+    return alias;
   },
   async items() {
-    const result = await get(`/v2/now/aliases/`)
-    return result.aliases
+    const result = await get(`/v2/now/aliases/`);
+    return result.aliases;
   },
-}
+};
 
 export const Alias = {
   async self({ source }) {
-    return root.aliases.one({ uid: source.uid })
+    return root.aliases.one({ uid: source.uid });
   },
-}
+};
 
 export const TeamsCollection = {
   async one({ args }) {
-    const result = await get(`/teams/${args.id}`)
-    return result
+    const result = await get(`/teams/${args.id}`);
+    return result;
   },
   async items() {
-    const result = await get(`/teams/`)
-    return result.teams
+    const result = await get(`/teams/`);
+    return result.teams;
   },
-}
+};
 
 export const Team = {
   async self({ source }) {
-    return root.teams.one({ id: source.id })
+    return root.teams.one({ id: source.id });
   },
-}
+};
